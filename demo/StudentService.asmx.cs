2d1ce82169487c059e3a3898faf5adea71203151
Tait Mdaka <Tait.Mdaka@avisbudget.co.za>
2018-07-25 15:25:47 +0200

    working soft ware need to be edidted

---
+++ demo/StudentService.asmx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+
+namespace demo
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public void GetAllStudents()
+        {
+            List<demo.student> listStudents = new List<student>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblStudents", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student student = new student();
+                    student.id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["Name"].ToString();
+                    student.gender = rdr["Gender"].ToString();
+                    student.city = rdr["City"].ToString();
+                    listStudents.Add(student);
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(listStudents));
+        }
+        [WebMethod]
+        public void GetStudent(int id)
+        {
+            student student = new student();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new
+                    SqlCommand("Select * from tblStudents where id = @id", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@id",
+                    Value = id
+                };
+
+                cmd.Parameters.Add(param);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student.id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["Name"].ToString();
+                    student.gender = rdr["Gender"].ToString();
+                    student.city = rdr["City"].ToString();
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(student));
+        }
+    }
+}
---
+++ demo/scripts/Script.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular
+    .module("Demo", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+            .when("/home", {
+                templateUrl: "Templates/home.html",
+                controller: "homeController"
+            })
+            .when("/students", {
+                templateUrl: "Templates/students.html",
+                controller: "studentsController"
+           
+            })
+          
+            //new route
+            .when("/students/:id", {
+                templateUrl: "Templates/StudentDetails.html",
+                controller: "studentDetailsController"
+            })
+
+
+        .otherwise({
+            redirectTo: "/home"
+        })
+
+        $locationProvider.html5Mode(true);
+    })
+    .controller("homeController", function ($scope) {
+        console.log('list mhome ');
+        $scope.message = "Home Page";
+
+    })
+
+    .controller("coursesController", function ($scope) {
+        console.log('list my student ');
+        $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+    })
+
+
+    .controller("studentsController", function ($scope, $http) {
+        //calling a web service 
+        console.log('list my student ');
+        $http.get("StudentService.asmx/GetAllStudents")
+            .then(function (response) {
+
+                $scope.students = response.data;
+            })
+
+    })
